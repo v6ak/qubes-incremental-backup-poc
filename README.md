4c5e979dbab2a5a61b27e19a182fa126073ba38f
Vít Šesták <user@fedora-21-dvm>
2017-02-19 12:28:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,142 @@
+## Status
+
+**Parts of implementation are missing. See the [“MVP” milestone](https://github.com/v6ak/qubes-incremental-backup-poc/issues?q=is%3Aopen+is%3Aissue+milestone%3AMVP) for details. Proof of concept. Backup format details will likely change.**
+
+## Goals
+
+* Incremental backups
+* Minimal backup size
+* Reasonably exploit-resistant. More specificaly:
+    * If filesystem of one VM is maliciously crafted, it does not affect other VMs or their backups, even in case of fs driver bug. And of course, it does not affect dom0 at all.
+    * All operations in dom0 are very careful about untrusted inputs, including master config, which is likely to be downloaded from an untrusted place.
+* Backup will be transfered over network to some untrusted place.
+* Attacker can obtain some reasonably limited metadata about the backup.
+* If attacker has modified the backup, it can be detected. Replay attacks (i.e., replacing by some older backup) are not currently intended to be mitigated, though. Attacker that controls the storage can also mix age of backups, remove some VM or add a removed VM.
+* bonus: Ability to backup a running VM (if easily achievable)
+
+## How to achieve it
+
+* File-based backup with exclusions of directories like ~/.cache. This allows small backup size and incremental backups.
+* Backup is performed of cloned VM images (shapshots on LVM). This allows repairing damaged FS before mounting. It also allows using backup on LVM shapshots while running.
+* Backups are both encrypted and authenticated using GPG.
+* I just write some scripts that glue some existing software for easy use with Qubes.
+
+## Where to run backup
+
+There are several place where one could run a backup:
+
+* dom0
+* Original VM
+* One dedicated backup VM
+* DVM
+
+The dom0 might sound as an obvious choice, because it has enough access to all data. But this is a wrong way once we need to parse some untrusted filesystem. While dom0 needs to be involved, parsing untrusted filesystem in dom0 is a no-go.
+
+The original VM might also sound as a reasonable choice, because it has access to all the data it needs. There are some minor (though non-negligible) security concerns like VM being able to read and modify its own backups. (This is mostly an issue when handling ransomware, but this is now what I am trying to strongly defend.) But there are also some practical issues. First is non-atomicity of reading the filesystem while the VM is running. For example, when a file is moved during backup, the backup can contain the file twice, once, or it might not contain the file at all. Second practical issue is: Should we shut the VM down after backing up? Maybe we should, because the user does not want it to be running. Maybe we should not, because the user is using the VM. Not shutting all VMs will cause OOM soon on a typical Qubes setup. Shutting all the VMs would close user's work sooner or later. Leaving previous state would be a good heuristic, but still not perfect. Most notably, user can start using a VM while backing up.
+
+Using one dedicated backup VM is no-go for similar reasons to dom0. The VM would be powerful (have plaintext access to content of other VMs) and exposed to attacks from crafted filesystem at the same time.
+
+The remaining option is a DVM, one per VM. This DVM would have access to a clone of filesystem of the relevant VM. If the DVM is compromised through a maliciously crafted filesystem, it does not have access to other VMs. Such exploit would have similar impact as compromised VM that takes care about its own backup. The need of exploiting a bug in filesystem parsing code is a serious mitigation factor, though. This also can allow using the VM when performing backup.
+
+The disadvantage of usage of DVM is that we will probably have to use a different approach for dom0 backups. But dom0 is so special case…
+
+## What software?
+
+I've tried few backup tools. I have decided to use Duplicity. This one was somehow usable and looks to be decently designed:
+
+* Opensource (=> publicly auditable)
+* Performs file-based incremental backups (full backup is performed once per a defined interval)
+* Backup is encrypted and authenticated using GPG. When one tampers the backup, it should be caught by GPG and the restore procedure should not continue.
+* Supports various storage backends.
+* Writing a custom storage backend seems to be a realistic amount of work: https://github.com/Rudd-O/duplicity-qubes
+* GPG can be configured. It can use either symmetric (preferred) or asymmetric cryptography.
+
+I anecdoticaly remember some issues with Duplicity, probably related with interrupted backup process. Maybe we should be careful there, but I don't know a good alternative for Duplicity at the moment.
+
+I haven't performed a deep review of Duplicity.
+
+If you want to use something else, I hope it will not be hard to replace it with some other suitable software. It looks like replacing Duplicity by some other software would require minor (if any) change to the dom0 part.
+
+## Keys for backups
+
+I believe it is clear that we need different keys for different VMs, unless we move the encryption and decryption to dom0. All keys are derived from one passphrase:
+
+master_key = stretch_key(passphrase)
+subkey_$vm = derive_subkey(master_key, "vm-"+vm)
+
+We also need few other subkeys, but they aren't so important.
+
+Key stretching is intentionally applied only once. Applying it for each VM separately would increase computational cost when running backup for multiple VMs, but it would not make legitimate users more secure against guessing the password. After guessing the password on one VM's backup, attacker would be able to reuse it on other VMs.
+
+Now, we have to define those two functions. Those two functions are meaned as configurable, so one could change them later. (The current implementation might not allow such degree of configurability, though.)
+
+### Key stretching
+
+Why we need this: Key size for encryption is usually enough, but passphrase is usually not as strong. If you use random characters from base64 without padding, you would need to remember 22 characters for equivalent of 128-bit keys or 44 characters for equivalent of 256-bit keys. Moreover, passphrases can be weakened by shouldersurfing. After observing several characters, attacker can try to guess others.
+
+I have picked scrypt, because it is tunable and fairly reviewed. Other functions (like bcrypt) could be also considered there.
+
+Parameters for scrypt are inspired from [https://stackoverflow.com/questions/11126315/what-are-optimal-scrypt-work-factors] . I have considered increasing parallelization parameter, but the implementation of scrypt I have used does not look like being able to use multiple cores. But we still might want to increate p and decrease N in order to lower memory requirements. The link suggests as a more paranoid version something like this:
+
+stretch_key(passphrase) = scrypt(passphrase, salt, 1<<20, 8, 1, 32)
+
+This requires 1GiB of RAM for scrypt, which is much memory in some cases. Since dom0 can have about 1GiB–4GiB of RAM, depending on requirements of other VMs, it can be a significant portion of total RAM available. I propose decresaing amount of needed memory with similar CPU requirements (similar time without parallelisation, lower memory requirements):
+
+stretch_key(passphrase) = scrypt(passphrase, salt, 1<<17, 8, 8, 32)
+
+Note that all the parameters are more strict than the soft variant recommended in the post mentioned above. While the recommendation is from 2009, the parameters are increased more than the Moore's inflation as of 2017. (In 8 years, computers are expected to be 2^4 times faster, while this is 2^6 times more hard. Moreover, the Moore's inflation does not hold for some parameters like RAM latency, which can slow the progress down, especially with Scrypt design.)
+
+Another advantage of this approach is that it can be faster for legitimate user when the implementation adds support for parallelism. Note that higher parallelisation does not help attacker when trying large number of passphrases, because it is ambarassimgly parallel task. That is, attacker can use multiple cores even for p=1. Sure, attacker needs fewer memory that with the former parameters. But that's the cost of making it nicer (requiring less memory) for legitimate users. And still, the attacker needs 128MiB of RAM, which is probably enough.
+
+The size 32 \[bytes\] was chosen because we can hardly go (or need to go) beyond 256-bit security.
+
+### Subkeys for VMs
+
+The subkey generation was designed to be fast. Key is already stretched:
+
+derive_subkey(master_key, subkey_identifier) = hmac(sha256, master_key, subkey_identifier)
+
+I know HMAC was designed for a different purpose. We need a keyed-PRF rather than MAC. But according to [https://cseweb.ucsd.edu/~mihir/papers/hmac-new.html], “HMAC is a PRF under the sole assumption that the compression function is a PRF”.
+
+TODO: Is SHA256 really a PRF? I hope so. And it also seems that it is used in such way in TLS: [https://crypto.stackexchange.com/questions/26410/whats-the-gcm-sha-256-of-a-tls-protocol]
+
+Probably even some punk solution like sha256(master_key || vm_name) would work well (length-extension attack is not practically applicable there), but I would preffer a more standard way.
+
+The sha256 function was chosen because of its output size length. Again, we can hardly go (or need to go) beyond 256-bit security.
+
+## File names
+
+It is tempting to use filenames simply derived from VM name. This would leak some metadata, though. The amount of leaked metadata is not huge, but still noticable. I argue that we should go on the safe side and obscure the metadata in some reasonable way.
+
+We use some kind of deterministic encryption. I have used a scheme similar to AES-SIV (which I have found no suitable implementation for), but I use HMAC-SHA256 instead of S2V. HMAC is simpler, already implemented and should do the same job there. The scheme (I call it AES-HIV) does not support AAD, but this is not a requirement.
+
+
+## Snapshots
+
+Snapshots are used for VM backup. The type of snapshot depends on the storage type of the VM. Two storage types are supported: plain file and LVM device.
+
+Plain files are simply copied. A check that the VM is not running is performed before the copy process is started. However, when the copy is in progress, the VM can be started, which can lead to some inconsistent copy. Yes, the situation is not nice, but I suppose that file-backed VMs aren't going to be there a long time, because Qubes 4 will reportedly use LVM.
+
+LVM block devices are handled by CoW snapshot. This is much faster and it works in some way even if the VM is running. Well… Actually, if you use a filesystem designed and configured for surviving sudden power outage, it should work well, because the snapshot of mounted filesystem will look like a filesystem where sudden power loss has happened. Practically speaking, if you are using ext4, you should have journal enabled and you shouldn't use flags like data=writeback.
+
+Note that Qubes 3.2 does not officially support LVM for private VM images. It is expected that private.img is a symlink to /dev/*&lt;vg>*/*&lt;lv>*. Other formats like /dev/mapper/*&lt;vg-with-doubled-minuses>*/*&lt;lv-with-doubled-minuses>* are not supported.
+
+## dom0 backup
+
+TODO
+
+## Limitations
+
+* DVM template is somehow trusted.
+* We assume that DVM template has drivers for all filesystems we need to backup
+* VM config backup is missing. User needs to backup `~/.v6-qubes-backup-poc/master` in order to be able to restore the backup.
+* The implementation assumes that attacker cannot read parameters of running applications (e.g., via /proc). This is justifiable in Qubes security model, especially in dom0, but it is not very nice.
+* VMs are identified by name. If you destroy a VM and create a VM of the same name then, it will use the same keys and it might have some security implications.
+* We strongly assume that attacker does not have any access to dom0. More specificaly, attacker cannot obtain a RAM snapshot of dom0 and attacker cannot list running applications of dom0 (e.g., via /proc). While those assumptions are standard in Qubes security model, failing to satisfy them can cause the backup security to break hardly. No serious attempt has been made to reduce number of copies of sensitive cryptographic material in RAM.
+* It assumes that we use locale utf-8. When one switches locale, it can cause issues, mostly with password. If you use another encoding, you might get troubles, especially if your password contains some characters that are represented differently in your encoding than in utf-8.
+* It assumes that user does not run the script multiple times in parallel. Various race conditions (cloned images, config files) could happen there.
+* (Probably incomplete)
+
+## Human aspects for password
+
+TODO:
---
+++ backup.py
@@ -0,0 +1,343 @@
+#!/bin/env python3
+# Preconditions: sudo qubes-dom0-update zenity perl-Crypt-ScryptKDF
+
+# The code is a PoC quality. It should generally work, but:
+# * Backup format and configuration can be changed without automatic migration.
+# * It probably violates some generally-accepted Python code style. I am sorry for that. I am new to Python. This is not a good excude for ugly code, but it is an excuse for suboptimal first version.
+# * It uses shell calls too much. Maybe it is evident that this code was originally written as Bash script.
+# * It does not use Qubes API, it calls shell commands instead and guesses some default paths. Maybe this will be fixed when ported for Qubes 4. Before switching to Qubes 4, we would get mix of Python 2 and Python 3.
+# * Exceptions are usually not caught. That is, you will probably get an ugly stacktrace rather than some friendly error message.
+
+import subprocess
+import base64
+import hmac
+import binascii
+import re
+import os
+import sys
+import shutil
+import collections
+from pathlib import Path
+
+def scrypt(password, salt, log2_N, r, p, length):
+	# I found no Python scrypt implementation in dom0 repository :(
+	return subprocess.check_output([
+		"perl",
+		"-e",
+		'use Crypt::ScryptKDF; use MIME::Base64; print(Crypt::ScryptKDF::scrypt_raw(decode_base64($ARGV[5]), decode_base64($ARGV[0]), 1<<($ARGV[1]+0), ($ARGV[2]+0), ($ARGV[3]+0), ($ARGV[4]+0)));',
+		"--",
+		base64.b64encode(salt),
+		str(log2_N),
+		str(r),
+		str(p),
+		str(length),
+		base64.b64encode(password.encode("utf-8"))
+	])
+
+# Encrypts or decrypts in AES-256 CTR mode. Encryption and decryption are the same. This is a low-level (pure) function where one has to specify IV.
+def aes256_ctr(key, iv, data):
+	assert len(key) == 32
+	assert len(iv) == 16
+	# crosstest: http://www.cryptogrium.com/aes-ctr.html
+	return subprocess.check_output(
+		["openssl", "enc", "-aes-256-ctr", "-K", binascii.hexlify(key), "-iv", binascii.hexlify(iv)],
+		input=data
+	)
+
+# AES-HIV is like AES-SIV, but it uses HMAC-SHA256 instead of S2V…
+
+def aes256_hiv_key_split(key):
+	assert(len(key) == 64)
+	key_crypt = key[:32]
+	key_mac = key[32:]
+	return [key_crypt, key_mac]
+
+def aes256_hiv_encrypt(key, plaintext, mac_size_bytes = 32):
+	assert(mac_size_bytes <= 32) # more bytes would not work well with SHA-256
+	assert(mac_size_bytes >= 16) # less bytes could cause problems with IV
+	[key_crypt, key_mac] = aes256_hiv_key_split(key)
+	mac = hmac.new(key_mac, plaintext, "sha256").digest()[:mac_size_bytes]
+	iv = mac[:16]
+	encrypted = aes256_ctr(key_crypt, iv, plaintext)
+	return mac + encrypted
+
+def aes256_hiv_decrypt(key, ciphertext, mac_size_bytes = 32):
+	assert(mac_size_bytes <= 32) # more bytes would not work well with SHA-256
+	assert(mac_size_bytes >= 16) # less bytes could cause problems with IV
+	[key_crypt, key_mac] = aes256_hiv_key_split(key)
+	mac_actual = ciphertext[:mac_size_bytes]
+	iv = mac_actual[:16]
+	encrypted = ciphertext[mac_size_bytes:]
+	plaintext_unverified = aes256_ctr(key_crypt, iv, encrypted)
+	mac_expected = hmac.new(key_mac, plaintext_unverified, "sha256").digest()[:mac_size_bytes]
+	if hmac.compare_digest(mac_expected, mac_actual):
+		return plaintext_unverified
+	else:
+		raise Exception("bad MAC")
+
+def ask_for_password(title):
+	return subprocess.check_output(["zenity", "--password", "--title="+title]).rstrip(b'\n').decode("utf-8")
+
+# Plaintext filename: Any case-sensitive utf-8 string without binary zeros (i.e. '\0') of size (in bytes) at most filename_plain_size
+# Ciphertext filename: A case-insensitive ascii base32 name of length base32_size_increase(filename_plain_size+mac_size)
+# Properties:
+# * Does not leak filename length
+# * The same plaintext filename is encrypted to the same ciphertext filename
+# * Related plaintext filenames are encrypted to unrelated ciphertext filenames
+# * Filenames are authenticated, i.e. attacker cannot abuse malleability
+# We use base32 in order to survive case-insensitive filesystem
+class FileNameCrypter:
+	def __init__(self, key, mac_size, filename_plain_size):
+		self.key = key
+		self.mac_size = mac_size
+		self.filename_plain_size = filename_plain_size
+	def encrypt(self, filename):
+		encoded_filename_unpadded = filename.encode("utf-8")
+		assert(encoded_filename_unpadded.find(b'\0') == -1)
+		encoded_filename_padded = encoded_filename_unpadded.ljust(self.filename_plain_size, b'\0')
+		assert(len(encoded_filename_unpadded) <= self.filename_plain_size)
+		assert(len(encoded_filename_padded) == self.filename_plain_size)
+		encrypted_filename_bytes = aes256_hiv_encrypt(self.key, encoded_filename_padded, self.mac_size)
+		return base64.b32encode(encrypted_filename_bytes).decode("ascii")
+	def decrypt(self, filename):
+		plain_padded = aes256_hiv_decrypt(self.key, base64.b32decode(filename.upper()), self.mac_size)
+		assert(len(plain_padded) == self.filename_plain_size )
+		plain = plain_padded.rstrip(b'\0')
+		assert(plain.find(b'\0') == -1)
+		return plain.decode("utf-8")
+
+class VmKeys(collections.namedtuple('VmKeys', 'encrypted_name key')): pass
+
+class MasterBackupSession:
+	def __init__(self, master_key, filename_mac_size = 16):
+		self.master_key = master_key
+		self.filename_mac_size = filename_mac_size
+		self.file_name_crypter = FileNameCrypter(self.subkey(b"files", 512), self.filename_mac_size, 96)
+	def subkey(self, subkey_id, length=256):
+		assert((length == 256) or (length == 512))
+		HASHES = {
+			256: "sha256",
+			512: "sha512"
+		}
+		return hmac.new(self.master_key, subkey_id, HASHES[length]).digest()
+	def subkey_vm(self, vm_name):
+		return self.subkey(("vm-"+vm_name).encode("utf-8"))
+	def test_master_key(self, test_content):
+		return hmac.compare_digest(test_content, self.gen_test_content())
+	def gen_test_content(self):
+		# Derive a testing key and MAC some hardcoded content :)
+		return hmac.new(self.subkey(b"test"), base64.b64decode("UMWZw61sacWhIMW+bHXFpW91xI1rw70ga8WvbiDDunDEm2wgxI/DoWJlbHNrw6kgw7NkeS4="), "sha256").digest()
+	def vm_keys(self, vm_name):
+		return VmKeys(
+			encrypted_name = self.file_name_crypter.encrypt(vm_name),
+			key = self.subkey_vm(vm_name)
+		)
+
+class Vm:
+	def __init__(self, name):
+		pattern = re.compile("\\A[a-zA-Z0-9-]+\\Z")
+		if not pattern.match(name):
+			raise Exception("bad name")
+		self.name = name
+	def is_running(self):
+		ret = subprocess.call(["qvm-check", "--running", self.name], stdout=subprocess.DEVNULL)
+		if ret == 0:
+			return True
+		elif ret == 1:
+			return False
+		else:
+			raise Exception("unexpected return code: "+str(ret))
+	def private_volume_path(self):
+		return Path("/var/lib/qubes/appvms/"+self.name+"/private.img")
+	def private_volume(self):
+		path = self.private_volume_path()
+		if path.is_symlink():
+			pat = re.compile("^/dev/([^/]+)/([^/]+)$")
+			location = os.readlink(str(path))
+			parts = pat.match(location)
+			if parts:
+				vg = parts.group(1)
+				lv = parts.group(2)
+				return LvmVolume(self, vg, lv)
+			else:
+				raise Exception("Cannot parse link to LVM volume: "+location)
+		elif path.is_file():
+			return FileVolume(self, path)
+		else:
+			raise Exception("unexpected file type")
+
+class DvmInstance: # We could extract VmInstance superclass, but we don't need it
+	def __init__(self, name):
+		self.name = name
+	@staticmethod
+	def create(color = "red"):
+		return DvmInstance(subprocess.check_output(["/usr/lib/qubes/qfile-daemon-dvm", "LAUNCH", "dom0", "", color]).decode("ascii").rstrip("\n"))
+	def close(self):
+		subprocess.check_output(["/usr/lib/qubes/qfile-daemon-dvm", "FINISH", self.name])
+	def attach(self, name, volume):
+		subprocess.check_output(["qvm-block", "--attach-file", self.name, volume.xen_path(), "-f", name])
+	def detach_all(self):
+		subprocess.check_output(["qvm-block", "--detach", self.name])
+	def check_output(self, command, stdin = None, input = None):
+		command = ["qvm-run", "-p", self.name, command]
+		if stdin == None:
+			return subprocess.check_output(command, input = input)
+		elif input == None:
+			return subprocess.check_output(command, stdin = stdin)
+		else:
+			raise Exception("cannot handle both stdin and input")
+
+class Volume:
+	def __init__(self, vm):
+		self.vm = vm
+	def clone(self):
+		raise Exception("not implemented")
+	def remove(self):
+		raise Exception("not implemented")
+	def xen_path(self):
+		raise Exception("not implemented")
+
+class LvmVolume(Volume):
+	def __init__(self, vm, vg, lv):
+		super().__init__(vm)
+		self.lv = lv
+		self.vg = vg
+	def __str__(self):
+		return "LvmVolume("+str(self.vm)+", "+str(self.lv)+", "+str(self.vg)+")"
+	def clone(self):
+		clone_lv = "clone-"+self.lv
+		clone_volume = LvmVolume(self.vm, self.vg, clone_lv)
+		if clone_volume.exists():
+			clone_volume.remove()
+		subprocess.check_output(["sudo", "lvcreate", "-L512M", "-s", "-n", clone_lv, self.lvm_path()])
+		return clone_volume
+	def remove(self):
+		subprocess.check_output(["sudo", "lvremove", "-f", self.file_path()])
+	def exists(self):
+		return os.path.exists(self.file_path())
+	def lvm_path(self):
+		return self.vg+"/"+self.lv
+	def file_path(self):
+		return "/dev/"+self.lvm_path()
+	def xen_path(self):
+		return "dom0:"+self.file_path()
+
+class FileVolume(Volume):
+	def __init__(self, vm, path):
+		super().__init__(vm)
+		self.path = path
+	def __str__(self):
+		return "FileVolume("+str(self.vm)+", "+str(self.path)+")"
+	def clone(self):
+		if self.vm.is_running():
+			raise Exception("VM is running, backup is not supported for this type of VM when running")
+		clone_path = Path(str(self.path)+".clone")
+		#shutil.copyfile(str(self.path), str(clone_path)) <— does not seem to support sparse files, so it is slooooooow
+		subprocess.check_output(["cp", "--sparse=always", "--", str(self.path), str(clone_path)])
+		return FileVolume(self.vm, clone_path)
+	def remove(self):
+		os.remove(str(self.path))
+	def xen_path(self):
+		return "dom0:"+str(self.path)
+
+class ConfigItem(collections.namedtuple('ConfigItem', 'default_generator')):
+	def default(self):
+		return self.default_generator()
+class SaltConfigItem(ConfigItem):
+	def read(self, inp):
+		res = inp.read(32)
+		assert(len(res) == 32)
+		assert(len(inp.read(1)) == 0) # check EOF
+		return res
+	def write(self, out, value):
+		out.write(value)
+
+class IntConfigItem(ConfigItem):
+	def read(self, inp):
+		res = inp.read(10) # up to 10 characters
+		assert(len(inp.read(1)) == 0) # check EOF
+		return int(res)
+	def write(self, out, value):
+		out.write(str(value).encode("ascii"))
+
+class BackupConfig:
+	def __init__(self, path, master_pass_config):
+		self.master_pass_config = master_pass_config
+		self.path = path
+	@staticmethod
+	def read_or_create(path):
+		# Maybe one could find an existing library that does this better. The problem is that the library would have to be reasonably reviewed, which is not an easy task.
+		conf = {
+			"salt": SaltConfigItem(lambda: os.urandom(32)),
+			"log2_N": IntConfigItem(lambda: 17),
+			"r": IntConfigItem(lambda: 8),
+			"p": IntConfigItem(lambda: 8)
+		}
+		res = {}
+		master_path = path / "master"
+		if not os.path.exists(str(master_path)):
+			os.makedirs(str(master_path))
+		for key, value_coder in conf.items():
+			f = master_path / key
+			if f.exists():
+				with open(str(f), "rb") as inp:
+					res[key] = value_coder.read(inp)
+			else:
+				with open(str(f), "wb+") as out:
+					value = value_coder.default()
+					value_coder.write(out, value)
+					res[key] = value
+		return BackupConfig(path, res)
+	def get_password_kdf(self):
+		return lambda password: scrypt(password, self.master_pass_config['salt'], self.master_pass_config['log2_N'], self.master_pass_config['r'], self.master_pass_config['p'], 32)
+	def _passphrase_test_file(self):
+		return self.path / "passphrase_test"
+	def passphrase_exists(self):
+		return os.path.exists(str(self._passphrase_test_file()))
+	def get_passphrase_test(self):
+		with open(str(self._passphrase_test_file()), "rb") as f: return f.read(64)
+	def save_passphrase_test(self, new_data):
+		with open(str(self._passphrase_test_file()), "wb+") as f: return f.write(new_data)
+
+def main():
+	if len(sys.argv) == 2:
+		vm = sys.argv[1]
+	else:
+		print("Usage: "+sys.argv[0]+" vm-name")
+		exit(1)
+
+	config = BackupConfig.read_or_create(Path(os.path.expanduser("~/.v6-qubes-backup-poc")))
+	# TODO: refactor password handling (repeated prompt when creating, repeated prompt when entering bad password, …)
+	kdf = config.get_password_kdf()
+	password = ask_for_password("Backup passphrase" if config.passphrase_exists() else "Create a new backup passphrase")
+	session = MasterBackupSession(kdf(password), 32)
+	password = None # just hygiene
+	if config.passphrase_exists():
+		if not session.test_master_key(config.get_passphrase_test()):
+			print("Bad password")
+			exit(66)
+	else:
+		config.save_passphrase_test(session.gen_test_content())
+	vm_keys = session.vm_keys(vm)
+
+	volume_clone = Vm(vm).private_volume().clone()
+	try:
+		dvm = DvmInstance.create()
+		try:
+			dvm.attach("xvdz", volume_clone)  # --ro: 1. is not needed since it is a clone, 2. blocks repair procedures when mounting
+			try:
+				dvm.check_output("sudo mkdir /mnt/clone")
+				dvm.check_output("sudo mount /dev/xvdz /mnt/clone") # TODO: consider -o nosuid,noexec – see issue #16
+				try:
+					with open(os.path.dirname(os.path.realpath(__file__))+"/vm-backup-agent", "rb") as inp:
+						dvm.check_output("cat > /tmp/backup-agent", stdin = inp)
+					dvm.check_output("chmod +x /tmp/backup-agent")
+					print(dvm.check_output("/tmp/backup-agent "+vm_keys.encrypted_name, input = vm_keys.key))
+					# TODO: also copy ~/.v6-qubes-backup-poc/master to the backup in order to make it recoverable without additional data (except password). See issue #12.
+				finally: dvm.check_output("sudo umount /mnt/clone")
+			finally: dvm.detach_all()
+		finally: dvm.close()
+	finally: volume_clone.remove()
+
+if __name__ == "__main__":
+	main()
